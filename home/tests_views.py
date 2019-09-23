3ef6df3f89d5ad6b5ffcb3233ae22572f61dd600
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2019-09-23 13:19:26 +0100

    test_views.py added to the home app

---
+++ home/tests_views.py
@@ -0,0 +1,32 @@
+from django.test import TestCase, Client
+
+c=Client()
+
+# Create your tests here.
+class TestAccountsViews(TestCase):
+    
+    def test_index_page(self):
+        page = c.get("/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "index.html")
+
+    def test_about_page(self):
+        page = c.get("/home/about/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "about.html")
+
+    def test_adonate_page(self):
+        page = c.get("/home/donate/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "donate.html")    
+    
+    def test_projects_page(self):
+        page = c.get("/home/projects/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "projects.html")    
+
+    def test_contact_page(self):
+        page = c.get("/home/contact/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "contact.html")    
+    
