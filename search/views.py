822d53dc0fa081ff94dc4876a5911a19d36d3938
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2019-09-25 15:00:54 +0100

    'q' changed to 'search' in the search form and summary replace with authot in views.py (search app)

--- search/views.py
+++ search/views.py
@@ -5,7 +5,7 @@ from django.db.models import Q
 # Create your views here.
 
 def do_search(request):
-    
-    books = Book.objects.filter(Q(title__icontains = request.GET['q']) | Q(summary__icontains = request.GET['q']))
+      
+    books = Book.objects.filter(Q(title__icontains = request.GET['search']) | Q(author__name__icontains = request.GET['search']))
     return render(request, 'all_books.html', {'books':books})
     
