8b0dc0b994bf1fb5d00f0422934ceb62b9b18f92
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2019-10-01 22:19:07 +0100

    fix issues in search app by including the categories in the view

--- search/views.py
+++ search/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from books.models import Book
+from books.models import Book, Category
 from django.db.models import Q
 
 # Create your views here.
 
 def do_search(request):
-      
-    books = Book.objects.filter(Q(title__icontains = request.GET['search']) | Q(author__name__icontains = request.GET['search']))
-    return render(request, 'all_books.html', {'books':books})
+    categories = Category.objects.filter(Q(book__title__icontains = request.GET['q']) | Q(book__author__name__icontains = request.GET['q'])).distinct()
+    books = Book.objects.filter(Q(title__icontains = request.GET['q']) | Q(author__name__icontains = request.GET['q']))
+    return render(request, 'all_books.html', {'books':books, 'categories':categories})
     
