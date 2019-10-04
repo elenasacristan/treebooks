24629a9c865fb1ac20556974027d662b8f2f51da
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2019-10-04 22:06:20 +0100

    favourites included to profile.html modal and heart icon working in all_books.html and detail.html

--- favourites/urls.py
+++ favourites/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import url
-from .views import add_remove_favourites, see_favourites
+from .views import add_remove_favourites
 
 urlpatterns = [
-    # url(r'^add/$', add_to_favourites, name='add_to_favourites'),
-    url(r'^$', see_favourites, name='see_favourites'),
     url(r'^addremove/(?P<id>\d+)$', add_remove_favourites, name='add_remove_favourites'),
 ]
--- favourites/views.py
+++ favourites/views.py
@@ -2,14 +2,10 @@ from django.shortcuts import render, reverse, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from userprofile.models import UserProfile
 from books.models import Book
+from django.http import HttpResponseRedirect
+
 
 # Create your views here.
-
-   
-def see_favourites(request):
-    userprofile = UserProfile.objects.get(user = request.user)
-    favourites = userprofile.favourites.all()
-    return render(request, 'favourites.html', {'favourites':favourites})
 
 '''
 if the book is already saved as favourite then
@@ -17,6 +13,7 @@ remove it from favourites.
 If the book is not saved as favourite then
 save it as favourite
 '''
+# https://stackoverflow.com/questions/12758786/redirect-return-to-same-previous-page-in-django/12758859
 def add_remove_favourites(request, id):
     book = get_object_or_404(Book,pk=id)
 
@@ -28,6 +25,8 @@ def add_remove_favourites(request, id):
             profile.favourites.remove(fav_book)
         else:
             profile.favourites.add(fav_book)
-    
-    return render(request, 'detail.html', {'book':book})
+    # print(request.get_full_path)
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+    # return render(request, 'detail.html', {'book':book})
 
