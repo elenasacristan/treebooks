b1959ae4e5865a3a70605d4ddcf248cb3ebdb18a
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2019-10-08 21:55:04 +0100

    waiting list books added to view_profile.html

--- waiting_list/urls.py
+++ waiting_list/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import join_waiting_list
+from .views import join_waiting_list, view_waiting_lists
 
 urlpatterns = [
-url (r'^(?P<pk>\d+)/$', join_waiting_list, name='join_waiting_list'),
+url (r'^(?P<pk>\d+)/$', view_waiting_lists, name='view_waiting_lists'),
+url (r'^join/(?P<pk>\d+)/$', join_waiting_list, name='join_waiting_list'),
 ]
--- waiting_list/views.py
+++ waiting_list/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, reverse, redirect, get_object_or_404
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from userprofile.models import UserProfile
 from books.models import Book
 from .models import WaitingList
@@ -9,6 +10,12 @@ from django.http import HttpResponseRedirect
 
 
 # Create your views here.
+@login_required
+def view_waiting_lists(request, pk):
+    waiting_list = WaitingList.objects.filter(wl_book__id = pk)
+    return render (request, 'waiting_list.html',{'waiting_list':waiting_list})
+
+@login_required
 def join_waiting_list(request, pk):
     add_wl_book = get_object_or_404(Book,pk=pk)
 
