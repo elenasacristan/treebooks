1e45c59bff7dbfd969a868a432920633264b8062
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2019-10-24 20:44:48 +0100

    code refactoring for test_forms and tests_views inside the accounts app

--- accounts/tests_forms.py
+++ accounts/tests_forms.py
@@ -3,6 +3,17 @@ from .forms import LoginForm,RegistrationForm
 
 class TestAccountsForms(TestCase):
 
+    '''set up the registration form to use in the test so we
+    don't have to repeat the setup in each test'''
+    def setUp(self):
+        self.registration_form = RegistrationForm({
+            'first_name':'first_name',
+            'last_name':'last_name', 
+            'email':'email@test.es', 
+            'username':'username', 
+            'password1':'12345abcds', 
+            'password2':'12345abcds'})
+
     def test_user_can_login_with_username_and_password(self):
         form = LoginForm({'username':'username', 'password':'password'})
         self.assertTrue(form.is_valid())
@@ -19,14 +30,7 @@ class TestAccountsForms(TestCase):
 
 
     def test_user_can_register_filling_all_the_fields(self):
-        form = RegistrationForm({
-            'first_name':'first_name',
-            'last_name':'last_name', 
-            'email':'email@test.es', 
-            'username':'username', 
-            'password1':'12345abcds', 
-            'password2':'12345abcds'})
-        self.assertTrue(form.is_valid())
+        self.assertTrue(self.registration_form.is_valid())
 
     def test_user_cannot_register_without_filling_out_all_fields(self):
         form = RegistrationForm({
@@ -67,16 +71,9 @@ class TestAccountsForms(TestCase):
         self.assertEqual(form.errors['password2'],[u'Please confirm your password'])
 
     def test_unique_email_in_registration(self):
-        form = RegistrationForm({
-            'first_name':'first_name',
-            'last_name':'last_name', 
-            'email':'email@test.es', 
-            'username':'username', 
-            'password1':'12345abcds', 
-            'password2':'12345abcds'})
-        self.assertTrue(form.is_valid())
-        
-        form.save()
+        self.assertTrue(self.registration_form.is_valid())
+        self.registration_form.save()
+
         form2 = RegistrationForm({
             'first_name':'first_name2',
             'last_name':'last_name2', 
@@ -88,16 +85,9 @@ class TestAccountsForms(TestCase):
         self.assertEqual(form2.errors['email'],[u'Email address must be unique'])
 
     def test_unique_username_in_registration(self):
-        form = RegistrationForm({
-            'first_name':'first_name',
-            'last_name':'last_name', 
-            'email':'email@test.es', 
-            'username':'username', 
-            'password1':'12345abcds', 
-            'password2':'12345abcds'})
-        self.assertTrue(form.is_valid())
-        
-        form.save()
+        self.assertTrue(self.registration_form.is_valid())
+        self.registration_form.save()
+
         form2 = RegistrationForm({
             'first_name':'first_name2',
             'last_name':'last_name2', 
@@ -107,14 +97,3 @@ class TestAccountsForms(TestCase):
             'password2':'12345abcds'})
         self.assertFalse(form2.is_valid())
         self.assertEqual(form2.errors['username'],[u'A user with that username already exists.'])
-
-
-    # def test_user_cannot_login_without_username(self):
-    #     form = LoginForm({'username':'', 'password':'password'})
-    #     self.assertFalse(form.is_valid())
-    #     self.assertEqual(form.errors['username'],[u'This field is required.'])
-
-    # def test_user_cannot_login_without_password(self):
-    #     form = LoginForm({'username':'username', 'password':''})
-    #     self.assertFalse(form.is_valid())
-    #     self.assertEqual(form.errors['password'],[u'This field is required.'])
--- accounts/tests_views.py
+++ accounts/tests_views.py
@@ -5,6 +5,17 @@ c=Client()
 
 # Create your tests here.
 class TestAccountsViews(TestCase):
+
+    '''set up the registration form to use in the test so we
+    don't have to repeat the setup in each test'''
+    def setUp(self):
+        self.registration_form = RegistrationForm({
+            'first_name':'first_name',
+            'last_name':'last_name', 
+            'email':'email@test.es', 
+            'username':'username', 
+            'password1':'12345abcds', 
+            'password2':'12345abcds'})
     
     def test_login_page(self):
         page = c.get("/accounts/login/")
@@ -19,25 +30,22 @@ class TestAccountsViews(TestCase):
         
 
     def test_login_functionality_user_registered(self):
-        user = RegistrationForm({"first_name":"first_name", "last_name":"last_name","username":"username", "email":"test@test.com", "password1":"qwer1234", "password2":"qwer1234"})
-        user.save()
+        self.registration_form.save()
             
-        login_ok = c.login(username="username", password="qwer1234")
+        login_ok = c.login(username="username", password="12345abcds")
         self.assertTrue(login_ok)
    
 
-    def test_login_functionality_user_not_registered(self):
-        user = RegistrationForm({"first_name":"first_name", "last_name":"last_name","username":"username", "email":"test@test.com", "password1":"qwer1234", "password2":"qwer1234"})
-        user.save()
+    def test_user_cannot_login_if_not_registered(self):
+        self.registration_form.save()
             
-        login_ok = c.login(username="otherusername", password="qwer1234")
+        login_ok = c.login(username="otherusername", password="12345abcds")
         self.assertFalse(login_ok)
 
     def test_logout_functionality(self):
-        user = RegistrationForm({"first_name":"first_name", "last_name":"last_name","username":"username", "email":"test@test.com", "password1":"qwer1234", "password2":"qwer1234"})
-        user.save()
+        self.registration_form.save()
             
-        login_ok = c.login(username="username", password="qwer1234")
+        login_ok = c.login(username="username", password="12345abcds")
         self.assertTrue(login_ok)
         
         page = c.get("/accounts/logout/", follow=True)
@@ -45,11 +53,10 @@ class TestAccountsViews(TestCase):
         self.assertRedirects(page, '/')
 
     
-    def test_user_cant_log_out_when_already_loged_out(self):
-        user = RegistrationForm({"first_name":"first_name", "last_name":"last_name","username":"username", "email":"test@test.com", "password1":"qwer1234", "password2":"qwer1234"})
-        user.save()
+    def test_user_cannot_log_out_when_already_loged_out(self):
+        self.registration_form.save()
             
-        login_ok = c.login(username="username", password="qwer1234")
+        login_ok = c.login(username="username", password="12345abcds")
         self.assertTrue(login_ok)
         
         page = c.logout()      
@@ -59,15 +66,20 @@ class TestAccountsViews(TestCase):
         self.assertRedirects(page, '/accounts/login/?next=/accounts/logout/')
 
     def test_authenticated_user_redirected_to_index_if_tries_to_log_in_again(self):
-        user = RegistrationForm({"first_name":"first_name", "last_name":"last_name","username":"username", "email":"test@test.com", "password1":"qwer1234", "password2":"qwer1234"})
-        user.save()
+        self.registration_form.save()
             
-        login_ok = c.login(username="username", password="qwer1234")
+        login_ok = c.login(username="username", password="12345abcds")
         self.assertTrue(login_ok)
         
         page = c.get("/accounts/login/", follow=True)
         self.assertEqual(page.status_code, 200)
         self.assertRedirects(page, '/')
     
-    
+    def test_user_can_login_using_email(self):
+        self.registration_form.save()
+            
+        login_ok = c.login(username="email@test.es", password="12345abcds")
+        self.assertTrue(login_ok)
+        
+       
     
