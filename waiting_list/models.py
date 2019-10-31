e06dace760006a6f389cfd6c11a6bfe274a2ec1d
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2019-10-31 22:37:41 +0000

    updated model WaitingList in order to remove date_joined field

--- waiting_list/models.py
+++ waiting_list/models.py
@@ -5,12 +5,8 @@ from django.utils import timezone
 
 # Create your models here.
 class WaitingList(models.Model):
-    date_joined = models.DateTimeField(blank=True, null=True, default=timezone.now)
     wl_book = models.OneToOneField(Book, related_name="related_book", on_delete=models.CASCADE)
     wl_user = models.ManyToManyField(User, related_name='users')
     
     def __str__(self):
-        return "{0} - {1}".format(self.date_joined.strftime('%m/%d/%Y'), self.wl_book)  
-
-    class Meta:
-       ordering = ('wl_book','date_joined',)
+        return self.wl_book.title  
