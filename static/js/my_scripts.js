d971dae1beb8876818391a9a56407bf91a69a26b
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2019-11-02 19:49:07 +0000

    commented my_scripts.js

--- static/js/my_scripts.js
+++ static/js/my_scripts.js
@@ -1,13 +1,19 @@
+// I learned by reading this post how to scroll automatically on hover
 // https://stackoverflow.com/questions/18188952/scroll-on-hover-click-for-speed
 
+// I learned and used the code from from the following link in order to
+// display a .gif icon while the page loads
 // https://smallenvelop.com/display-loading-icon-page-loads-completely/
 
+
 $(document).ready(function() {
+  // code use to display loaded
   $(window).load(function() {
     // Animate loader off screen
     $(".loader").fadeOut("slow");
   });
 
+  // code used to autoscroll books in all_books.html
   var count;
   var interval;
 
