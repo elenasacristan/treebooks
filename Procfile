2c3136fb307f3a5d1c242d2a0a7067eb93756fda
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2019-11-09 14:51:22 +0000

    Profile added. Preparing project for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn treebooks.wsgi:application
--- treebooks/settings.py
+++ treebooks/settings.py
@@ -125,13 +125,6 @@ WSGI_APPLICATION = 'treebooks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# REMOVE ONCE READY TO DEPLOY
-# if "DATABASE_URL" in os.environ:
-#         DATABASES = {
-#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#         }
-
-
 
 # in development
 if development==True:
