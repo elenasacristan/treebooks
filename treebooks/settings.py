7c9a3d0567c1a40ada4d6c39443b06f62ffedca6
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2019-11-14 23:46:46 +0000

    update settings to fix issue sending invites from heroku

--- treebooks/settings.py
+++ treebooks/settings.py
@@ -244,22 +244,25 @@ STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
 
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = os.getenv('SENDGRID_API_KEY')
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
-
 #These are the variables needed to send email to reset the password
-# DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_ADDRESS")
-# SERVER_EMAIL = os.environ.get("EMAIL_ADDRESS")
 
-# EMAIL_USE_TLS = True
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
-# EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
-# EMAIL_PORT = 587
+if development==True:
+    DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_ADDRESS")
+    SERVER_EMAIL = os.environ.get("EMAIL_ADDRESS")
+
+    EMAIL_USE_TLS = True
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
+    EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+    EMAIL_PORT = 587
+
+else:
+    EMAIL_HOST = 'smtp.sendgrid.net'
+    EMAIL_HOST_USER = 'apikey'
+    EMAIL_HOST_PASSWORD = os.getenv('SENDGRID_API_KEY')
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 '''
