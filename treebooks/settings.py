541edc96dbe5165cb2065d634cabb19a6e52b5dc
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2023-10-01 10:50:59 +0100

    Set development to True for testing

--- treebooks/settings.py
+++ treebooks/settings.py
@@ -33,6 +33,9 @@ else:
     development = False
 
 
+#for testing
+development = True
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
